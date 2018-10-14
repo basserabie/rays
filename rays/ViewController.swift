9f4331f8d2b1d33de7e85128a0d955f597e28fa3
Darren Basserabie <yishai.b@yeshivalearners.co.za>
2018-10-14 09:48:25 +0200

    Initial Commit

---
+++ rays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rays
+//
+//  Created by Yishai Basserabie on 2018/10/14.
+//  Copyright © 2018 Yishai Basserabie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
